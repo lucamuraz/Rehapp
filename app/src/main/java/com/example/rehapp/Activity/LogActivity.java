942fdb1d95a799f1bacc27303962bccab944ac38
doc <enrico.piras@edu.unito.it>
2020-10-20 18:15:33 +0200

    LogActivity commit

--- app/src/main/java/com/example/rehapp/Activity/LogActivity.java
+++ app/src/main/java/com/example/rehapp/Activity/LogActivity.java
@@ -1,15 +1,20 @@
 package com.example.rehapp.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.rehapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,12 +26,11 @@ import java.util.Iterator;
 import java.util.Map;
 
 
-
 public class LogActivity extends AppCompatActivity{
+
     private static final String TAG = "info";
     FirebaseDatabase mDB;
     Map<String, Object> map = new HashMap<>();
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +38,15 @@ public class LogActivity extends AppCompatActivity{
         setContentView(R.layout.activity_log);
     }
 
+
     public void back(View view) {
         Intent i = new Intent(this,MainActivity.class);
         startActivity(i);
     }
-
     public void btnRegistrati(View view) {
         Intent i = new Intent(this, RegisterActivity.class);
         startActivity(i);
     }
-
     public void accedi(View view) {
         EditText u = findViewById(R.id.username);
         EditText p = findViewById(R.id.password);
@@ -52,28 +55,32 @@ public class LogActivity extends AppCompatActivity{
         loginUser(username,password);
     }
 
-    //in prova non funzionante.
+
     private void loginUser(final String username, final String password) {
         initDatabase();
-        final DatabaseReference myRef = mDB.getReference("Utenti").child(username);
-        Log.i(TAG,myRef.getKey());
 
+        final DatabaseReference myRef = mDB.getReference("Utenti").child(username);
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 map =(Map<String, Object>) dataSnapshot.getValue();
                 Log.i(TAG, "Value is: " + map);
-                Iterator i = map.entrySet().iterator();
+                Iterator it = map.entrySet().iterator();
                 String u = null;
-                while(i.hasNext()){
-                    Map.Entry  p =(Map.Entry) i.next();
+                while(it.hasNext()){
+                    Map.Entry  p =(Map.Entry) it.next();
                     if(map.containsKey("Password")) {
                         u = p.getValue().toString();
                     }
                     Log.i(TAG, "Value is: " + u); // legge la password.
-                }
 
+                }
+                if(username.equals(myRef.getKey()) && password.equals(u)){
+                    Intent i = new Intent(getApplicationContext(),Home.class);
+                    i.putExtra("username", username);
+                    startActivity(i);
+                }
                 myRef.removeEventListener(this);
             }
             @Override
@@ -82,14 +89,6 @@ public class LogActivity extends AppCompatActivity{
                 Log.i(TAG, "Failed to read value.", error.toException());
             }
         });
-        /*
-        if(username.equals(myRef.getKey()) && password.equals(u)){
-            Intent i = new Intent(this, Home.class);
-            i.putExtra("usarname", username);
-            startActivity(i);
-        }
-         */
-
     }
     public void initDatabase(){
         mDB = FirebaseDatabase.getInstance();
