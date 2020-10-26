a6a4b9287950f5ca9f623eef7545f317d81f282a
doc <enrico.piras@edu.unito.it>
2020-10-26 10:22:19 +0100

    Register and model commit (i file start e start 1 sono stati sotituiti da i file : RegFragment 1, 2, 3).

---
+++ app/src/main/java/com/example/rehapp/SaveSharedPreferences.java
@@ -0,0 +1,99 @@
+package com.example.rehapp;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class SaveSharedPreferences{
+
+    static final String PREF_USER_NAME= "name";
+    static final String PREF_USER_SURNAME= "surname";
+    static final String PREF_USER_EMAIL= "email";
+    static final String PREF_USER_PSW= "password";
+    static final String PREF_USER_EDSS= "EDSS";
+    static final String PREF_USER= "user";
+
+    static final String ACTIVITY_TYPE="type";
+
+
+    static SharedPreferences getSharedPreferences(Context ctx) {
+        return PreferenceManager.getDefaultSharedPreferences(ctx);
+    }
+
+    public static void setUserName(Context ctx, String userName) {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(PREF_USER_NAME, userName);
+        editor.apply();
+    }
+
+    public static void setUserSurname(Context ctx, String userSurname) {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(PREF_USER_SURNAME, userSurname);
+        editor.apply();
+    }
+
+    public static void setUserEmail(Context ctx, String userEmail) {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(PREF_USER_EMAIL, userEmail);
+        editor.apply();
+    }
+
+    public static void setUserPassword(Context ctx, String userPassword) {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(PREF_USER_PSW, userPassword);
+        editor.apply();
+    }
+
+    public static void setUserEdss(Context ctx, String userEdss) {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(PREF_USER_EDSS, userEdss);
+        editor.apply();
+    }
+
+    public static void setActivityType(Context ctx, String type) {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(ACTIVITY_TYPE, type);
+        editor.apply();
+    }
+
+    public static void setUser(Context ctx, String user) {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.putString(PREF_USER, user);
+        editor.apply();
+    }
+
+    public static String getUserName(Context ctx) {
+        return getSharedPreferences(ctx).getString(PREF_USER_NAME, "");
+    }
+
+    public static String getUser(Context ctx) {
+        return getSharedPreferences(ctx).getString(PREF_USER, "");
+    }
+
+    public static String getUserSurname(Context ctx) {
+        return getSharedPreferences(ctx).getString(PREF_USER_SURNAME, "");
+    }
+
+    public static String getUserEmail(Context ctx) {
+        return getSharedPreferences(ctx).getString(PREF_USER_EMAIL, "");
+    }
+
+    public static String getUserPassword(Context ctx) {
+        return getSharedPreferences(ctx).getString(PREF_USER_PSW, "");
+    }
+
+    public static String getUserEdss(Context ctx) {
+        return getSharedPreferences(ctx).getString(PREF_USER_EDSS, "");
+    }
+
+    public static String getActivityType(Context ctx) {
+        return getSharedPreferences(ctx).getString(ACTIVITY_TYPE, "");
+    }
+
+    public static void clearData(Context ctx) {
+        SharedPreferences.Editor editor = getSharedPreferences(ctx).edit();
+        editor.clear(); //clear all stored data
+        editor.apply();
+    }
+
+}
